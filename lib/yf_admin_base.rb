e147a97adbf521a7ff5df32e64a23e0dce9541cc
y.fujii <ishikurasakura@gmail.com>
2015-04-21 12:47:59 +0900

    refinementsを定義

--- lib/yf_admin_base.rb
+++ lib/yf_admin_base.rb
@@ -1,4 +1,5 @@
 require "yf_admin_base/engine"
+require "yf_admin_base/refinements/string_ex"
 
 module YfAdminBase
 end
---
+++ lib/yf_admin_base/refinements/string_ex.rb
@@ -0,0 +1,17 @@
+module YfAdminBase::Refinements
+  module StringEx
+    refine String do
+      def numeric?
+        (self.to_s =~ /^\d+$/).present?
+      end
+
+      def has_alphabet?
+        !!(Regexp.new(/[a-zA-Z]/) =~ self.to_s)
+      end
+
+      def has_numeric?
+        !!(Regexp.new(/[0-9]/) =~ self.to_s)
+      end
+    end
+  end
+end
