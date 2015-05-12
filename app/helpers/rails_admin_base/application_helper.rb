fef9ff059213fe5a4afbfd58fd8cba8d529482ea
y.fujii <ishikurasakura@gmail.com>
2015-05-12 17:24:17 +0900

    enum添え字でoptionを作るメソッド追加

--- app/helpers/rails_admin_base/application_helper.rb
+++ app/helpers/rails_admin_base/application_helper.rb
@@ -30,5 +30,10 @@ module RailsAdminBase
         [klass.human_attribute_name(key), key]
       end
     end
+
+    def options_for_select_from_enum_number(klass, target)
+      enum_list = klass.send("#{target.to_s}s")
+      enum_list.keys.map{|key| klass.human_attribute_name(key)}.zip(enum_list.values)
+    end
   end
 end
