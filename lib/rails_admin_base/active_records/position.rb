e24439c32faf81899af50beb4dcccdda2a8b0e95
y.fujii <ishikurasakura@gmail.com>
2015-05-20 16:59:45 +0900

    missing_numberを複数カラム対応

--- lib/rails_admin_base/active_records/position.rb
+++ lib/rails_admin_base/active_records/position.rb
@@ -42,5 +42,62 @@ module RailsAdminBase::ActiveRecord
 
       ActiveRecord::Base.connection.execute(sql)
     end
+
+    def get_missing_number_from_numbering_scope_constant(target_column_name: 'position')
+      placeholder, where_value = get_where_and_assign_value
+
+      sql = <<-"SQL_EOS".strip_heredoc
+          SELECT
+            MIN( #{target_column_name} + 1 ) AS #{target_column_name}
+          FROM
+            #{self.class.table_name}
+          WHERE
+              #{where_value} AND
+              (#{target_column_name} + 1) NOT IN (SELECT #{target_column_name} FROM #{self.class.table_name} where #{where_value})
+        SQL_EOS
+
+      sql = ActiveRecord::Base.send(:sanitize_sql_array, [sql, placeholder])
+      results = self.class.find_by_sql(sql)
+
+      raise GetMissingNumberFailer if results.size != 1
+
+      results.first[target_column_name].to_i
+    end
+
+    def renumbering_from_numbering_scope_constant(target_column_name: 'position')
+      placeholder, where_value = get_where_and_assign_value
+
+      where_value = "where (#{where_value})" if where_value.present?
+
+      # 自動整列
+      sql = <<-"SQL_EOS".strip_heredoc
+          update #{self.class.table_name}
+            inner join
+              ( select * from
+                (
+                  select @i:=@i+1 as rownum,id,#{target_column_name} from (select @i:=0) as dummy, #{self.class.table_name}
+                  #{where_value}
+                  order by #{target_column_name}
+                ) as auto_numbering
+              ) as row_number
+            on
+              #{self.class.table_name}.id = row_number.id
+           set #{self.class.table_name}.#{target_column_name} = row_number.rownum
+      SQL_EOS
+
+      sql = ActiveRecord::Base.send(:sanitize_sql_array, [sql, placeholder])
+      ActiveRecord::Base.connection.execute(sql)
+    end
+
+    def get_where_and_assign_value
+      placeholder = {}
+      where_values  = []
+      self.class::NUMBERING_SCOPE.each do |scope|
+        where_values << "`#{self.class.table_name}`.`#{scope}` = :#{scope}"
+        placeholder[scope] = send(scope)
+      end
+      where_value = where_values.join(' AND ')
+      [placeholder, where_value]
+    end
   end
 end
