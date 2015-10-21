7ef0153f4811e18c98436be709749632eabf3fbf
yoshitsugu_fujii <ishikurasakura@gmail.com>
2015-10-21 16:15:09 +0900

    異なる値をもつキーを抽出するメソッドを定義

--- lib/rails_admin_base/refinements/hash_ex.rb
+++ lib/rails_admin_base/refinements/hash_ex.rb
@@ -1,6 +1,16 @@
 module RailsAdminBase::Refinements
   module HashEx
     refine Hash do
+      def different_values_key(hash)
+        keys = []
+        me = self.with_indifferent_access
+        hash = hash.with_indifferent_access
+        me.keys.each do |key|
+          next unless hash.has_key?(key)
+          keys << key unless hash[key] == me[key]
+        end
+        keys
+      end
     end
   end
 end
